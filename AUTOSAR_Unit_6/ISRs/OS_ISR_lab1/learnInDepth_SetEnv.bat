30bae93a3c59009bf1066144f2ecda2a38cbe2ff
Sasa <ahmed382002@gmail.com>
2024-10-27 00:37:13 +0300

    Finished OS ISRs management

---
+++ AUTOSAR_Unit_6/ISRs/OS_ISR_lab1/learnInDepth_SetEnv.bat
@@ -0,0 +1,8 @@
+@echo off
+SET ARM_CROSS_TOOLCHAIN_PATH=D:\Learn_In_Depth\Tools\ARM\bin
+SET PYTHON_PATH=C:\Users\User\AppData\Local\Programs\Python\Python312\
+SET goil_PATH=..\..\
+
+SET PATH=%ARM_CROSS_TOOLCHAIN_PATH%;%PYTHON_PATH%;%goil_PATH%;%PATH%
+
+echo Environment variables have been set.
